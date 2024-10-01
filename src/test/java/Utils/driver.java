1d2ac411d0fc23c0739d06b85f294a37e560b494
Ilkin Mamiyev Ilqar <ilkin.mamiyev@accessbank.az>
2024-10-01 08:39:29 +0400

    added headless mode to utils/driver

--- src/test/java/Utils/driver.java
+++ src/test/java/Utils/driver.java
@@ -1,6 +1,6 @@
 package Utils;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,15 +9,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class driver {
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
         WebDriverManager.chromedriver().setup();
-        ChromeOptions wd = new ChromeOptions();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // Отключение уведомлений
         Map<String, Object> prefs = new HashMap<>();
         prefs.put("profile.default_content_setting_values.notifications", 2);
-        wd.setExperimentalOption("prefs", prefs);
-        wd.addArguments("--remote-allow-origins=*");
+        options.setExperimentalOption("prefs", prefs);
 
-        ChromeDriver testDriver = new ChromeDriver(wd);
+        // Необходимо для совместимости с последними версиями Chrome
+        options.addArguments("--remote-allow-origins=*");
+
+        // Проверяем, если тесты запускаются в CI среде (например, GitHub Actions)
+        String headlessEnv = System.getenv("CI");
+        if (headlessEnv != null && headlessEnv.equals("true")) {
+            options.addArguments("--headless");  // Headless режим
+            options.addArguments("--disable-gpu");  // Отключение GPU
+            options.addArguments("--window-size=1920,1080");  // Задание размера окна
+            options.addArguments("--no-sandbox");  // Обход некоторых ограничений в Linux
+            options.addArguments("--disable-dev-shm-usage");  // Использование RAM вместо диска
+        }
+
+        // Создание экземпляра ChromeDriver
+        ChromeDriver testDriver = new ChromeDriver(options);
         testDriver.manage().window().maximize();
         return testDriver;
     }
